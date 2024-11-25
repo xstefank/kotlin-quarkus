9588b7f6b32babcf60a19d05e51f6faedc5cc2fd
xstefank <xstefank122@gmail.com>
2024-11-25 17:12:41 +0100

    Add testing project

---
+++ testing/src/test/kotlin/org/acme/GreetingResourceTest.kt
@@ -0,0 +1,22 @@
+package org.acme
+
+import io.quarkus.logging.Log
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class GreetingResourceTest {
+
+    @Test
+    fun testHelloEndpoint() {
+        Log.error("Running ${javaClass.name}")
+        given()
+          .`when`().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(`is`("Hello from Quarkus REST"))
+    }
+
+}
